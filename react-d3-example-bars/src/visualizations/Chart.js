e29d14014b7d82a37a68a96c59dd650acde14821
Forest Altherr <faltherr1@gmail.com>
2019-06-16 14:42:30 -0600

    SImple transition managed by d3

--- react-d3-example-bars/src/visualizations/Chart.js
+++ react-d3-example-bars/src/visualizations/Chart.js
@@ -7,8 +7,13 @@ const margin = { top: 20, right: 5, bottom: 20, left: 35 };
 
 class Chart extends Component {
   state = {
-    bars: []
+    bars: [],
+    xScale: ''
   };
+
+  xAxis = d3.axisBottom()
+  yAxis = d3.axisLeft()
+    // .scale(yScale)
 
   static getDerivedStateFromProps(nextProps, prevState) {
     const { data } = nextProps;
@@ -17,11 +22,11 @@ class Chart extends Component {
     let tempExtent = d3.extent(data, d=>d.high)
     let xScale = d3.scaleTime()
       .domain(d3.extent(data, d=> d.date))
-      .range([0, width])
+      .range([margin.left, width - margin.right])
       
     let yScale = d3.scaleLinear()
       .domain([Math.min(tempExtent[0], 0), tempExtent[1]])
-      .range([height,0])
+      .range([height - margin.bottom,margin.top])
 
     let colorScale = d3.scaleSequential()
       .domain(d3.extent(data, d => d.avg).reverse())
@@ -35,16 +40,41 @@ class Chart extends Component {
         fill: colorScale(el.avg)
       }
     })
-    return {bars}
+    return {bars, xScale, yScale}
+  }
+
+  componentDidUpdate(){
+    // D3 draws our axis
+    this.xAxis.scale(this.state.xScale)
+    d3.select(this.refs.xAxis).call(this.xAxis)
+    this.yAxis.scale(this.state.yScale)
+    d3.select(this.refs.yAxis).call(this.yAxis)
+
+    // D3 transitions animating y height and fill
+    d3.select(this.refs.bars)
+      .selectAll('rect')
+      .data(this.state.bars)
+      .transition()
+      .attr('y', d=> d.y)
+      .attr('height', d=> d.height)
+      .attr('fill', d=> d.fill)
   }
 
   render() {
     console.log(this.state.bars)
     return (
       <svg width={width} height={height}>
-        {this.state.bars.map(bar=>{
-          return <rect x={bar.x} y={bar.y} width={2} height={bar.height} fill={bar.fill}/>
-        })}
+        <g ref="bars">
+          {this.state.bars.map((bar,i)=>{
+            // return <rect key={i} x={bar.x} y={bar.y} width={2} height={bar.height} fill={bar.fill}/>
+            // To animate bars in react we want to make sure that d3 does not control the same things that React does
+            // Here we drop y fill and height because our d3 transition calculation will handle this part of the DOM
+            return <rect key={i} x={bar.x} width={2}/>
+          })}
+        </g>
+        <g ref='xAxis' transform={`translate(0,${height-margin.bottom})`}/>
+        <g ref='yAxis' transform={`translate(${margin.left})`}/>
+
       </svg>
       );
   }
