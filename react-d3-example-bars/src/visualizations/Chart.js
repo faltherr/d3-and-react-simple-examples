878d293b1c6a685c4485833b8a2102288251dc2f
Forest Altherr <faltherr1@gmail.com>
2019-06-16 12:44:30 -0600

    Basic bar chart that reacts to user input

--- react-d3-example-bars/src/visualizations/Chart.js
+++ react-d3-example-bars/src/visualizations/Chart.js
@@ -12,13 +12,37 @@ class Chart extends Component {
 
   static getDerivedStateFromProps(nextProps, prevState) {
     const { data } = nextProps;
+    console.log(data)
     if (!data) return {};
+    let tempExtent = d3.extent(data, d=>d.high)
+    let xScale = d3.scaleTime()
+      .domain(d3.extent(data, d=> d.date))
+      .range([0, width])
+      
+    let yScale = d3.scaleLinear()
+      .domain([Math.min(tempExtent[0], 0), tempExtent[1]])
+      .range([height,0])
 
-    return {};
+    let bars = data.map(el =>{
+      return{
+        x: xScale(el.date),
+        y: yScale(el.high),
+        height: yScale(el.low)-yScale(el.high),
+        fill: 'black'
+      }
+    })
+    return {bars}
   }
 
   render() {
-    return <svg width={width} height={height} />;
+    console.log(this.state.bars)
+    return (
+      <svg width={width} height={height}>
+        {this.state.bars.map(bar=>{
+          return <rect x={bar.x} y={bar.y} width={2} height={bar.height}/>
+        })}
+      </svg>
+      );
   }
 }
 
